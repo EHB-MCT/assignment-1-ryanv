c8cb20c8946ddfb817b8d67913148c23fdc2a948
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-05 20:14:19 +0200

    feat: importing pygame and other necessary libraries in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+
+# Imports
+import pygame
+import sys
+from pygame.locals import *
+import random
+import time
+from datetime import datetime
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, BLACK
+from game import run_game
+
+# Initialise Pygame
+pygame.init()
+
