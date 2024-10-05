3fb38d3d2108c509d983481e016e97cb9998d4c8
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-05 20:21:02 +0200

    feat: loading Game class

---
+++ src/game.py
@@ -0,0 +1,11 @@
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.player = Player()
+
+    def update(self):
+        self.player.update()
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        self.player.draw(self.screen)
