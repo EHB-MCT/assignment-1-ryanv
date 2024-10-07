606d9ff4ab0b94be70fa649c1f99f5eefe698590
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-07 11:55:07 +0200

    docs: add documentation for player movement

--- src/player.py
+++ src/player.py
@@ -8,12 +8,15 @@ class Player:
         self.rect.y = 100
         self.speed = 5
 
+        # Movement attributes (boolean) declaration
         self.moving_left = False
         self.moving_right = False
         self.moving_up = False
         self.moving_down = False
 
+    # Movement controls event listeners
     def handle_event(self, event):
+        # Set movement attributes to True when key pressed down
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 self.moving_left = True
@@ -24,6 +27,7 @@ class Player:
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 self.moving_down = True
 
+        # Set movement attributes back to False when key is released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 self.moving_left = False
@@ -34,6 +38,7 @@ class Player:
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 self.moving_down = False
 
+    # Update player position if any movement attribute is set to True
     def update(self):
         if self.moving_left:
             self.rect.x -= self.speed
@@ -44,5 +49,6 @@ class Player:
         if self.moving_down:
             self.rect.y += self.speed
 
+    # Draw player on screen
     def draw(self, screen):
         screen.blit(self.image, self.rect)
