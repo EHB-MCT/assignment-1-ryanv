10a984bf51a0897f576f3f8cdc501a6ffbbd8e87
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-19 14:51:37 +0200

    docs: adding in line documentation to Game class

--- src/game.py
+++ src/game.py
@@ -13,19 +13,29 @@ class Game:
         self.create_enemies(3) #number of enemies
 
     def create_enemies(self, number_of_enemies):
+        # Get player size from player image rect
         player_size = (self.player.rect.width, self.player.rect.height)
 
+        # Add enemies to the list
         for _ in range(number_of_enemies):
             enemy = Enemy(self.screen.get_width(), self.screen.get_height(), player_size)
             self.enemies.append(enemy)
 
     def update(self):
+        # Update player position
         self.player.update()
 
+        # Update enemy positions
         for enemy in self.enemies:
             enemy.update()
 
     def draw(self):
+        # Fill screen with a black background
         self.screen.fill((0, 0, 0))
         self.player.draw(self.screen)        for enemy in self.enemies:
+
+        # Draw player
+        self.player.draw(self.screen)
+
+        # Draw enemies
             enemy.draw(self.screen)
