65d51536ba7b7615942f9ac4906c7e6976d621a7
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-19 14:51:00 +0200

    feat: adding enemies to game class to generate them on screen

--- src/game.py
+++ src/game.py
@@ -1,12 +1,31 @@
 from player import Player
+from enemy import Enemy
+import random
+
+
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.player = Player()
 
+        # Create a list of enemies
+        self.enemies = []
+        self.create_enemies(3) #number of enemies
+
+    def create_enemies(self, number_of_enemies):
+        player_size = (self.player.rect.width, self.player.rect.height)
+
+        for _ in range(number_of_enemies):
+            enemy = Enemy(self.screen.get_width(), self.screen.get_height(), player_size)
+            self.enemies.append(enemy)
+
     def update(self):
         self.player.update()
 
+        for enemy in self.enemies:
+            enemy.update()
+
     def draw(self):
         self.screen.fill((0, 0, 0))
-        self.player.draw(self.screen)
+        self.player.draw(self.screen)        for enemy in self.enemies:
+            enemy.draw(self.screen)
