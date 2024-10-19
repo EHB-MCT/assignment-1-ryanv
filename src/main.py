069d880b33b7075d79f70cb82d3131f3a6519956
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-10-19 15:55:45 +0200

    refactor(main): remove unnecessary imports

--- src/main.py
+++ src/main.py
@@ -3,10 +3,6 @@
 # Imports
 import pygame
 import sys
-from pygame.locals import *
-import random
-import time
-from datetime import datetime
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, BLACK
 from game import Game  # Import the Game class
 
